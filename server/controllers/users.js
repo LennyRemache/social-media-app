d87c9aa6e84f2a11a05b58c8518aa30c12a2ba29
LennyRemache <lenny.j.remache@gmail.com>
2023-01-12 16:59:48 -0500

    Set Up User controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -35,3 +35,43 @@ export const getUserFriends = async (req, res) => {
     res.status(404).json({ msg: err.message });
   }
 };
+
+/* UPDATE */
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    // remove friend
+    if (user.friends.includes(friendId)) {
+      user.friends.filter((id) => id !== friendId);
+      friend.friends.filter((id) => id !== id);
+    } else {
+      // add friend
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, location, picturePath }) => {
+        return {
+          _id,
+          firstName,
+          lastName,
+          location,
+          picturePath,
+        };
+      }
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ msg: err.message });
+  }
+};
